d4a923149a97e00dc42e0486d0d6c0fd1b3062cc
MadridMaverick <thienlt.7@gmail.com>
2023-04-13 16:15:52 +0700

    update chatgpt question format

--- ChatGPT.py
+++ ChatGPT.py
@@ -39,26 +39,31 @@ class Chatbot(OpenAIAPI):
     def __init__(self, purpose='songs', config=None, default_user_id=None):
         super().__init__(config, default_user_id)
         if purpose == 'songs':
-            self.beginning_prompt = 'The following paragraph(s) describes an image.'
-            self.end_prompt = '\n\nRecommend a list of {n_songs} songs that would be the most suitable background music of that image. \
-            The moods of the song should be {moods}; the genres of the song should be {genres}. \
-                It is more important for the content of the songs to match than for the titles of the songs to match. \
-                    Please provide your answers as a list of JSON-like format (and say nothing else) that have 2 attributes: "title", "artist".'
+            self.beginning_prompt = 'The following paragraph(s) describes an image.\n\n'
+            self.end_prompt1 = '\n\nRecommend a list of {n_songs} songs that would be the most suitable background music of that image. '
+            self.end_prompt2 = \
+            'It is more important for the content of the songs to match than for the titles of the songs to match. '\
+            'Please provide your answers as a list of JSON-like format (and say nothing else) that have 2 attributes: "title", "artist".'
             self.lastSpotifyCall = None
             self.access_token = None
             self.token_type = None
 
-    def recommendSong(self, n_songs, caption: str, moods: list, genres: list):
+    def recommendSong(self, n_songs, caption: str, moods: list = None, genres: list = None):
         # progress = tqdm.tqdm(len(captions), ncols=75, desc='Collect ChatGPT')
-        with open('chatgpt_songs.json', 'w') as f:
-            f.write('')
+        # with open('chatgpt_songs.json', 'w') as f:
+        #     f.write('')
 
         self.delete_conversation()
         answer = None
 
+        mood_phrase = "The moods of the songs should be {mood_list}. ".format(
+            mood_list=", ".join(moods)) if moods is not None else ""
+        genre_phrase = "The genres of the songs should be {genre_list}. ".format(
+            genre_list=", ".join(genres)) if genres is not None else ""
         question = self.beginning_prompt + caption + \
-            self.end_prompt.format(n_songs=n_songs, moods=", ".join(
-                moods), genres=", ".join(genres))
+            self.end_prompt1.format(n_songs=n_songs) + \
+            mood_phrase + genre_phrase + self.end_prompt2
+
         _, answer, _ = self.ask_stream(question)
         answer = re.sub('  +', '', answer.strip().replace('\n', ''))
 
@@ -101,8 +106,4 @@ if __name__ == "__main__":
     genres = ["ballad", "piano"]
     config = getGPTReady()
     chatbot = Chatbot(config=config)
-    _ = chatbot.recommendSong(
-        n_songs=2, caption=caption, moods=moods, genres=genres)
-    print()
-    print("========================================")
-    print(_)
+    _ = chatbot.recommendSong(n_songs=2, caption=caption, moods=moods, genres=genres)
